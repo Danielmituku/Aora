2c0bb1f74458633d9861cb9b756d177112d1d97c
DanieMituku <dberhe490@gmail.com>
2024-07-11 11:42:03 +0300

    a new files added

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 
+
 export default function App() { 
   return (
     <View className="flex-1 items-center justify-center bg-white">
