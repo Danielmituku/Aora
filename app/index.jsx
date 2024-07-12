a45752bbe5d3f08747c3517779e2c4ba3c4add26
DanieMituku <dberhe490@gmail.com>
2024-07-12 09:31:05 +0300

    changes mage

--- app/index.jsx
+++ app/index.jsx
@@ -6,10 +6,10 @@ import { Link } from 'expo-router';
 
 export default function App() { 
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
+    <View>
+      <Text>Aora!</Text>
       <StatusBar style="auto" />
-      <Link href='/home' style={{color: 'blue'}}>Home</Link>
+      <Link href='/home' style={{color: 'blue'}}>Go to profile</Link>
     </View>
   );
 }
