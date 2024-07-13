a5ef3e7cd9ee428b150fd81b51e274f0381a8935
DanieMituku <dberhe490@gmail.com>
2024-07-13 12:33:26 +0300

    update

--- app/lib/appwrite.js
+++ app/lib/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Account, ID } from 'react-native-appwrite';
+import { Client, Account, ID, Avatars, Databases } from 'react-native-appwrite';
 
 export const config = {
     endpoint: 'https://cloud.appwrite.io/v1',
@@ -20,13 +20,43 @@ client
 ;
 
 const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
 
 // Register User
-export const createUser = () => {
-    account.create(ID.unique(), 'me@example.com', 'password', 'Jane Doe')
-    .then(function (response) {
-        console.log(response);
-    }, function (error) {
-        console.log(error);
-    });
+export const createUser = async (email, password, username) => {
+ try {
+    const newAccount = await account.create(
+        ID.unique(), email, password, username
+    )
+    if(! newAccount) throw Error;
+    const avatarURL = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+        config.databaseId,
+        config.userCollectionId,
+        ID.unique(),
+        {
+            accountId: newAccount.$id,
+            username,
+            avatar: avatarURL
+        }
+    )
+    return newUser;
+ } 
+ 
+ catch (error) {
+    console.log(error)
+    throw new Error(error);
+ }
+}
+
+export async function signIn(email, password){
+    try {
+        const session = await account.createEmailSession(email, password)
+    } catch (error) {
+        throw new Error(error);
+    }
 }
